74e60d8b2c41010017a2591e80c4c6ad7b882e39
Oksana Karpyn <oksana101992@gmail.com>
2023-08-20 15:08:14 +0200

    add html e css

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const calendarBox = document.querySelectorAll('.calendar-box');
+const calendarIcon = document.querySelectorAll('.calendar-icon');
+let boxOpened = document.querySelector('.box-opened');
+let modalHidden = document.querySelector('.modal-hidden');
+const modal = document.querySelector('.modal');
+const modalWindow = document.querySelector('.modal-window');
+const modalContent = document.querySelector('.modal-content');
+const modalButton = document.querySelector('.modal-button');
+
